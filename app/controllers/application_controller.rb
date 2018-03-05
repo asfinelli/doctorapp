861f4a3fe604cfe9538df5a2551f282a54b3bc26
Drew Finelli <asfinelli@gmail.com>
2018-03-04 20:30:48 -0500

    code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :password_confirmation, :username, :name, :dob, :phone_number, :gender, :address, :city, :state, :postal_code, :country])
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
 
   devise_for :doctors
+
   devise_for :users
+
+
   authenticated :user do
     root to: 'user_sessions#show'
   end
+
+
   authenticated :doctor do
     root to: 'doctor_sessions#show'
   end
 
+
   resources :user_sessions
+
+
   resources :doctor_sessions
+
+
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
