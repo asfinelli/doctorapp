1b39b037917feb26972943f244a59e41357f36da
Drew Finelli <asfinelli@gmail.com>
2018-03-06 11:04:26 -0500

    styling

--- app/controllers/doctor_sessions_controller.rb
+++ app/controllers/doctor_sessions_controller.rb
@@ -4,4 +4,7 @@ class DoctorSessionsController < ApplicationController
   def show
     @doctor = current_doctor
   end
+  def index
+    @appointment = Appointment.where(doctor_id: current_doctor.id)
+  end
 end
