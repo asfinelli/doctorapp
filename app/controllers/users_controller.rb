d9a34f23175614527d84bbbb3a4eb83b9d4b4043
Drew Finelli <asfinelli@gmail.com>
2018-03-14 14:22:22 -0400

    updated patient history ajax function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_doctor!
-  def index
 
- @users = User.search(params[:search]).order(:name).paginate(:per_page => 5, :page => params[:page])
+  def index
+    @users = User.search(params[:search]).order(:name).paginate(:per_page => 5, :page => params[:page])
+
   end
 
 
-def show
-  @user = User.find(params[:id])
-  @appointments = @user.appointments.collect
-end
-end
+  def show
+    @user = User.find(params[:id])
+    @appointments = @user.appointments.collect
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js
+    end
+  end
+  end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,12 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor, optional: true
+  acts_as_messageable
 
-acts_as_messageable
-
+private
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(1_000_000)
+    end while Model.where(id: self.id).exists?
+  end
 end
