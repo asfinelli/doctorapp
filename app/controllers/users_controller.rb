94bf7d96988e012785a7abd84269ecfff465689e
Drew Finelli <asfinelli@gmail.com>
2018-03-16 12:31:16 -0400

    updated patient database view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,16 @@ class UsersController < ApplicationController
   before_action :authenticate_doctor!
 
   def index
-    @users = User.search(params[:search]).order(:name).paginate(:per_page => 10, :page => params[:page])
+    @users = User.all.order('name ASC')
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js
   end
+end
 
 
   def show
+
     @user = User.find(params[:id])
     @appointments = @user.appointments.collect
     respond_to do |format|
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
   acts_as_messageable
+  before_save :capitalize_names
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
+  def capitalize_names
+    self.name = name.camelcase
+  end
   def mailboxer_name
     self.name
   end
