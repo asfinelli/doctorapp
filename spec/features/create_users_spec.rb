7028284c761fbf36aa4ca2562bec43e72acea4c5
Drew Finelli <asfinelli@gmail.com>
2018-04-05 14:13:14 -0400

    cleaning up code

--- spec/features/create_users_spec.rb
+++ spec/features/create_users_spec.rb
@@ -1,10 +1,10 @@
-# require 'rails_helper.rb'
-#
-# feature 'Creating Users' do
-#   scenario 'can create user' do
-#     visit '/'
-#     click_link 'New User'
-#     visit 'new_user_registration'
-#
-#   end
-# end
+require 'rails_helper.rb'
+
+feature 'Creating Users' do
+  scenario 'can create user' do
+    visit '/'
+    click_link 'New User'
+    visit 'new_user_registration'
+
+  end
+end
